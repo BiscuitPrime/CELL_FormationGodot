8fd51cd5da2356c44478251ee570f1d3cbf1f0fa
BiscuitPrime <henri.nomico@gmail.com>
2022-04-04 15:00:56 +0200

    Added modular health for enemies + cookies

--- Source/Scripts/Actors/Enemy.gd
+++ Source/Scripts/Actors/Enemy.gd
@@ -1,8 +1,9 @@
-extends KinematicBody2D
+extends Actors
 #This script is used by the enemies
 #Author : Henri 'Biscuit Prime' Nomico
 
 onready var anim = $AnimatedSprite
+export var health : int
 
 func _ready():
 	anim.animation="idle"
@@ -13,4 +14,13 @@ func hit():
 	anim.animation="hit"
 	anim.play()
 	yield(anim,"animation_finished")
-	queue_free()
+	anim.animation="idle"
+	anim.play()
+	health-=1
+	if(health<=0):
+		queue_free()
+
+func _process(delta):
+	_velocity.y += gravity * get_physics_process_delta_time()
+	_velocity = move_and_slide(_velocity,FLOOR_NORMAL)
+	pass
