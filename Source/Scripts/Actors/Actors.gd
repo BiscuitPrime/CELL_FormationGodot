bc4d0e6577138104f1a2050f2226348dd514a3a3
BiscuitPrime <henri.nomico@gmail.com>
2022-04-07 13:23:52 +0200

    Removed all non-essential code and scenes

--- Source/Scripts/Actors/Actors.gd
+++
@@ -1,9 +0,0 @@
-class_name Actors
-extends KinematicBody2D
-#This script is used by the actors
-
-var gravity := 1000
-var speed = Vector2(300,500)
-var _velocity := Vector2.ZERO
-var _direction := Vector2.ZERO
-var FLOOR_NORMAL := Vector2.UP
